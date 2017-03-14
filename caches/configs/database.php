fefa092dbdc44ac32aa6aa1f4bb35914ffab0616
xiangxiang0826 <xiangxiang0826@126.com>
2017-03-14 09:47:44 +0800

    add
    add

---
+++ caches/configs/database.php
@@ -0,0 +1,18 @@
+<?php
+
+return array (
+	'default' => array (
+		'hostname' => 'localhost',
+		'database' => 'aihao',
+		'username' => 'root',
+		'password' => '888888',
+		'tablepre' => 'v9_',
+		'charset' => 'utf8',
+		'type' => 'mysql',
+		'debug' => true,
+		'pconnect' => 0,
+		'autoconnect' => 0
+		),
+);
+
+?>
---
+++ caches/error_log.php
@@ -0,0 +1,44 @@
+<?php exit;?>05-26 11:47:02 | 8192 | Function split() is deprecated | caches\caches_model\caches_data\content_update.class.php | 70
+<?php exit;?>05-26 11:54:16 | 8192 | Function split() is deprecated | caches\caches_model\caches_data\content_update.class.php | 70
+<?php exit;?>05-26 11:55:40 | 8192 | Function split() is deprecated | caches\caches_model\caches_data\content_update.class.php | 70
+<?php exit;?>05-26 11:55:52 | 2 | fsockopen(): php_network_getaddresses: getaddrinfo failed: 不知道这样的主机。  | phpcms\libs\classes\http.class.php | 84
+<?php exit;?>05-26 11:55:52 | 2 | fsockopen(): unable to connect to tool.phpcms.cn:80 (php_network_getaddresses: getaddrinfo failed: 不知道这样的主机。 ) | phpcms\libs\classes\http.class.php | 84
+<?php exit;?>05-26 11:56:43 | 8192 | Function split() is deprecated | caches\caches_model\caches_data\content_update.class.php | 70
+<?php exit;?>05-26 11:57:28 | 8192 | Function split() is deprecated | caches\caches_model\caches_data\content_update.class.php | 70
+<?php exit;?>05-26 11:58:31 | 8192 | Function split() is deprecated | caches\caches_model\caches_data\content_update.class.php | 70
+<?php exit;?>05-26 12:00:38 | 8192 | Function split() is deprecated | caches\caches_model\caches_data\content_update.class.php | 70
+<?php exit;?>05-26 12:01:59 | 8192 | Function split() is deprecated | caches\caches_model\caches_data\content_update.class.php | 70
+<?php exit;?>05-26 12:02:45 | 8192 | Function split() is deprecated | caches\caches_model\caches_data\content_update.class.php | 70
+<?php exit;?>05-26 12:14:32 | 8192 | Function split() is deprecated | caches\caches_model\caches_data\content_update.class.php | 70
+<?php exit;?>05-26 12:15:17 | 8192 | Function split() is deprecated | caches\caches_model\caches_data\content_update.class.php | 70
+<?php exit;?>05-26 12:15:59 | 8192 | Function split() is deprecated | caches\caches_model\caches_data\content_update.class.php | 70
+<?php exit;?>05-26 12:24:14 | 2 | extract() expects parameter 1 to be array, null given | phpcms\modules\content\classes\html.class.php | 182
+<?php exit;?>05-27 17:48:12 | 2 | fsockopen(): php_network_getaddresses: getaddrinfo failed: 不知道这样的主机。  | phpcms\libs\classes\http.class.php | 84
+<?php exit;?>05-27 17:48:12 | 2 | fsockopen(): unable to connect to tool.phpcms.cn:80 (php_network_getaddresses: getaddrinfo failed: 不知道这样的主机。 ) | phpcms\libs\classes\http.class.php | 84
+<?php exit;?>05-31 17:48:42 | 2 | fsockopen(): php_network_getaddresses: getaddrinfo failed: 不知道这样的主机。  | phpcms\libs\classes\http.class.php | 84
+<?php exit;?>05-31 17:48:42 | 2 | fsockopen(): unable to connect to tool.phpcms.cn:80 (php_network_getaddresses: getaddrinfo failed: 不知道这样的主机。 ) | phpcms\libs\classes\http.class.php | 84
+<?php exit;?>05-31 17:52:47 | 8192 | Function split() is deprecated | caches\caches_model\caches_data\content_update.class.php | 70
+<?php exit;?>05-09 21:47:31 | 2 | mysql_connect() [<a href='function.mysql-connect'>function.mysql-connect</a>]: [2002] 由于目标计算机积极拒绝，无法连接。
+ (trying to connect via tcp://localhost:3306) | phpcms\libs\classes\mysql.class.php | 56
+<?php exit;?>05-09 21:47:31 | 2 | mysql_connect() [<a href='function.mysql-connect'>function.mysql-connect</a>]: 由于目标计算机积极拒绝，无法连接。
+ | phpcms\libs\classes\mysql.class.php | 56
+<?php exit;?>05-09 21:47:31 | 2 | mysql_error() expects parameter 1 to be resource, boolean given | phpcms\libs\classes\mysql.class.php | 388
+<?php exit;?>05-09 21:47:31 | 2 | mysql_errno() expects parameter 1 to be resource, boolean given | phpcms\libs\classes\mysql.class.php | 392
+<?php exit;?>05-09 21:47:31 | 2 | mysql_errno() expects parameter 1 to be resource, boolean given | phpcms\libs\classes\mysql.class.php | 392
+<?php exit;?>05-09 21:47:31 | 2 | mysql_error() expects parameter 1 to be resource, boolean given | phpcms\libs\classes\mysql.class.php | 388
+<?php exit;?>05-12 22:15:59 | 2 | mysql_connect() [<a href='function.mysql-connect'>function.mysql-connect</a>]: [2002] 由于目标计算机积极拒绝，无法连接。
+ (trying to connect via tcp://localhost:3306) | phpcms\libs\classes\mysql.class.php | 56
+<?php exit;?>05-12 22:15:59 | 2 | mysql_connect() [<a href='function.mysql-connect'>function.mysql-connect</a>]: 由于目标计算机积极拒绝，无法连接。
+ | phpcms\libs\classes\mysql.class.php | 56
+<?php exit;?>05-12 22:15:59 | 2 | mysql_error() expects parameter 1 to be resource, boolean given | phpcms\libs\classes\mysql.class.php | 388
+<?php exit;?>05-12 22:15:59 | 2 | mysql_errno() expects parameter 1 to be resource, boolean given | phpcms\libs\classes\mysql.class.php | 392
+<?php exit;?>05-12 22:15:59 | 2 | mysql_errno() expects parameter 1 to be resource, boolean given | phpcms\libs\classes\mysql.class.php | 392
+<?php exit;?>05-12 22:15:59 | 2 | mysql_error() expects parameter 1 to be resource, boolean given | phpcms\libs\classes\mysql.class.php | 388
+<?php exit;?>06-19 09:15:54 | 2 | mysql_connect() [<a href='function.mysql-connect'>function.mysql-connect</a>]: [2002] 由于目标计算机积极拒绝，无法连接。
+ (trying to connect via tcp://localhost:3306) | phpcms\libs\classes\mysql.class.php | 56
+<?php exit;?>06-19 09:15:54 | 2 | mysql_connect() [<a href='function.mysql-connect'>function.mysql-connect</a>]: 由于目标计算机积极拒绝，无法连接。
+ | phpcms\libs\classes\mysql.class.php | 56
+<?php exit;?>06-19 09:15:54 | 2 | mysql_error() expects parameter 1 to be resource, boolean given | phpcms\libs\classes\mysql.class.php | 388
+<?php exit;?>06-19 09:15:54 | 2 | mysql_errno() expects parameter 1 to be resource, boolean given | phpcms\libs\classes\mysql.class.php | 392
+<?php exit;?>06-19 09:15:54 | 2 | mysql_errno() expects parameter 1 to be resource, boolean given | phpcms\libs\classes\mysql.class.php | 392
+<?php exit;?>06-19 09:15:54 | 2 | mysql_error() expects parameter 1 to be resource, boolean given | phpcms\libs\classes\mysql.class.php | 388
