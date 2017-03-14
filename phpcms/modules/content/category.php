ab079a309d70696a44cffadd8bedc18692cc2a99
xiangxiang0826 <xiangxiang0826@126.com>
2017-03-14 10:14:56 +0800

    add
    add

---
+++ phpcms/modules/content/category.php
@@ -0,0 +1,35 @@
+<?php
+defined('IN_PHPCMS') or exit('No permission resources.');
+//模型缓存路径
+define('CACHE_MODEL_PATH',CACHE_PATH.'caches_model'.DIRECTORY_SEPARATOR.'caches_data'.DIRECTORY_SEPARATOR);
+pc_base::load_app_func('util','content');
+class category {
+	private $db;
+	function __construct() {
+		$this->db = pc_base::load_model('content_model');
+		$this->_userid = param::get_cookie('_userid');
+		$this->_username = param::get_cookie('_username');
+		$this->_groupid = param::get_cookie('_groupid');
+	}
+	//首页
+	public function init() {
+		if(isset($_GET['siteid'])) {
+			$siteid = intval($_GET['siteid']);
+		} else {
+			$siteid = 1;
+		}
+		$siteid = $GLOBALS['siteid'] = max($siteid,1);
+		define('SITEID', $siteid);
+		$_userid = $this->_userid;
+		$_username = $this->_username;
+		$_groupid = $this->_groupid;
+		//SEO
+		$SEO = seo($siteid);
+		$sitelist  = getcache('sitelist','commons');
+		$default_style = $sitelist[$siteid]['default_style'];
+		$CATEGORYS = getcache('category_content_'.$siteid,'commons');
+                //echo $default_style;exit;
+		include template('content','category',$default_style);
+	}
+}
+?>
---
+++ phpsso_server/caches/caches_admin/caches_data/applist.cache.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'phpcms_v9',
+    'name' => 'phpcms v9',
+    'url' => 'http://aihao.com/',
+    'authkey' => 'GhGk8hienlSI5yzUgN9EEo9GG2R9Kb5F',
+    'ip' => '',
+    'apifilename' => 'api.php?op=phpsso',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+  ),
+);
+?>
